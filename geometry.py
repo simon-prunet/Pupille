a5351e4d6039cc24c993e6e8ad3b4f2aadb8e2d4
simon-prunet <prunet@gmail.com>
2026-01-20 14:48:50 +0100

    Gray pupil computed from polygonal construct

--- geometry.py
+++ geometry.py
@@ -2,7 +2,10 @@ import numpy as np
 import shapely
 import shapely.geometry as geom
 from shapely.ops import unary_union
-from shapely.geometry import Polygon
+from shapely.geometry import Polygon, box, Point, MultiPolygon
+from shapely.affinity import rotate
+from shapely.prepared import prep
+
 args = {'circular_resolution': 360, 'pupil_diameter': 800, 'obscuration_diameter': 110, 'spider_width': 50, 'spider_angle': 45.0,
         'offset_x': 20.0}
 
@@ -18,9 +21,9 @@ class PupilGeometry:
         self.horizontal_offset = self.offset_x * np.array([1, -1, -1, 1])
         
     def create_pupil(self):
-        outer_circle = geom.Point(0, 0).buffer(self.pupil_diameter / 2, resolution=self.circular_resolution)
+        outer_circle = Point(0, 0).buffer(self.pupil_diameter / 2, resolution=self.circular_resolution)
         if self.obscuration_diameter > 0:
-            inner_circle = geom.Point(0, 0).buffer(self.obscuration_diameter / 2, resolution=self.circular_resolution)
+            inner_circle = Point(0, 0).buffer(self.obscuration_diameter / 2, resolution=self.circular_resolution)
             pupil_shape = outer_circle.difference(inner_circle) 
         else:
             pupil_shape = outer_circle
@@ -38,7 +41,7 @@ class PupilGeometry:
                     (x_offset + self.pupil_diameter / 2 + 10, self.spider_width / 2),
                     (x_offset, self.spider_width / 2)
                 ])
-                spider = shapely.affinity.rotate(spider, angle, origin=(x_offset,0))
+                spider = rotate(spider, angle, origin=(x_offset,0))
                 spiders.append(spider)
 
             spiders_union = unary_union(spiders)
@@ -52,14 +55,40 @@ class PupilGeometry:
     def get_perimeter(self):
         pupil_shape = self.create_pupil()
         return pupil_shape.length
-    def plot_pupil(self):
+    def plot_pupil(self, outer_scale_factor=1.1):
         import matplotlib.pyplot as plt
         pupil_shape = self.create_pupil()
-        x, y = pupil_shape.exterior.xy
         plt.figure(figsize=(6,6))
-        plt.fill(x, y, color='black')
-        plt.xlim(-self.pupil_diameter/2 - 50, self.pupil_diameter/2 + 50)
-        plt.ylim(-self.pupil_diameter/2 - 50, self.pupil_diameter/2 + 50)
+
+        if isinstance(pupil_shape, MultiPolygon):
+            for poly in pupil_shape.geoms:
+                x, y = poly.exterior.xy
+                plt.fill(x, y, color='gray')
+        else:               
+            x, y = pupil_shape.exterior.xy
+            plt.fill(x, y, color='gray')
+        plt.xlim(-self.pupil_diameter/2*outer_scale_factor, self.pupil_diameter/2*outer_scale_factor)
+        plt.ylim(-self.pupil_diameter/2*outer_scale_factor, self.pupil_diameter/2*outer_scale_factor)
         plt.gca().set_aspect('equal', adjustable='box')
         plt.title('Pupil Geometry')
         plt.show()
+
+    def create_pixel_weights(self, num_pixels, outer_scale_factor=1.1):
+        pupil_shape = self.create_pupil()
+        pupil_prepared = prep(pupil_shape)
+        radius = (self.pupil_diameter / 2) * outer_scale_factor
+        x = np.linspace(-radius, radius, num_pixels+1)
+        y = np.linspace(-radius, radius, num_pixels+1)
+        pixel_weights = np.zeros((num_pixels, num_pixels))
+
+        pixels = []
+        for i in range(num_pixels):
+            for j in range(num_pixels):
+                pixel = box(x[i], y[j], x[i+1], y[j+1])
+                pixels.append(pixel)
+                if pupil_prepared.intersects(pixel):
+                    pixel_weights[i, j] = pupil_shape.intersection(pixel).area / pixel.area
+                else:
+                    pixel_weights[i, j] = 0.0
+        return x, y, pixel_weights
+    
