7834b772d83fdade12a06b129f9dbe3e78ba3176
simon-prunet <prunet@gmail.com>
2026-01-23 15:12:32 +0100

    Pupil is more realistic, with proper symmetries. Need to implement shear matrix (stretching along a given axis)

--- geometry.py
+++ geometry.py
@@ -6,19 +6,18 @@ from shapely.geometry import Polygon, box, Point, MultiPolygon
 from shapely.affinity import rotate
 from shapely.prepared import prep
 
-args = {'circular_resolution': 360, 'pupil_diameter': 800, 'obscuration_diameter': 110, 'spider_width': 50, 'spider_angle': 45.0,
-        'offset_x': 20.0}
+# args = {'circular_resolution': 360, 'pupil_diameter': 800, 'obscuration_diameter': 112, 'spider_width': 40, 'spider_angle': 50.5,
+#        'x_offset': 111}
 
 class PupilGeometry:
     def __init__(self, **kwargs):
         self.pupil_diameter = kwargs.get('pupil_diameter', 800)
-        self.obscuration_diameter = kwargs.get('obscuration_diameter', 110)
-        self.spider_width = kwargs.get('spider_width', 50)
-        self.spider_angle = kwargs.get('spider_angle', 45.0)
-        self.offset_x = kwargs.get('offset_x', 20.0)
+        self.obscuration_diameter = kwargs.get('obscuration_diameter', 112)
+        self.spider_width = kwargs.get('spider_width', 4) 
+        self.spider_angle = kwargs.get('spider_angle', 50.5)
+        self.x_offset = kwargs.get('x_offset', 111.0/2)
         self.circular_resolution = kwargs.get('circular_resolution', 360)
-        self.num_spiders = 4
-        self.horizontal_offset = self.offset_x * np.array([1, -1, -1, 1])
+        self.horizontal_offset = self.x_offset * np.array([1, -1, -1, 1])
         
     def create_pupil(self):
         outer_circle = Point(0, 0).buffer(self.pupil_diameter / 2, resolution=self.circular_resolution)
@@ -28,11 +27,11 @@ class PupilGeometry:
         else:
             pupil_shape = outer_circle
 
-        if self.num_spiders > 0 and self.spider_width > 0:
+        if self.spider_width > 0:
             spiders = []
-            angle_between_spiders = 360 / self.num_spiders
-            for i in range(self.num_spiders):
-                angle = self.spider_angle + i * angle_between_spiders
+            angles = [self.spider_angle, 180.-self.spider_angle, 180+self.spider_angle, -self.spider_angle]
+            for i in range(4):
+                angle = angles[i]
                 x_offset = self.horizontal_offset[i]
 
                 spider = Polygon([
@@ -81,11 +80,9 @@ class PupilGeometry:
         y = np.linspace(-radius, radius, num_pixels+1)
         pixel_weights = np.zeros((num_pixels, num_pixels))
 
-        pixels = []
         for i in range(num_pixels):
             for j in range(num_pixels):
                 pixel = box(x[i], y[j], x[i+1], y[j+1])
-                pixels.append(pixel)
                 if pupil_prepared.intersects(pixel):
                     pixel_weights[i, j] = pupil_shape.intersection(pixel).area / pixel.area
                 else:
